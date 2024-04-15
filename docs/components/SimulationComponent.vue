2edcb475a6c7caa72803b6897522db67ee39f169
not-shoyo <akheelachu@gmail.com>
2024-04-15 15:04:18 +0530

    merge conflicts

--- docs/components/SimulationComponent.vue
+++ docs/components/SimulationComponent.vue
@@ -5,10 +5,9 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
-// import { GUI } from "dat.gui";
+import { GUI } from "dat.gui";
 
 console.log("inside Simulation Component Script")
-
 
 // Determine if it's a mobile layout
 const isMobile = ref(false);
